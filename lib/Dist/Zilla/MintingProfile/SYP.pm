10b21afdc4f3ede1e5a690870d4e31b745d6901d
Станислав Пусеп <stas@sysd.org>
2012-12-04 15:08:40 +0000

    Import of SYP/Dist-Zilla-MintingProfile-SYP-0.006 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-MintingProfile-SYP
    gitpan-cpan-version:      0.006
    gitpan-cpan-path:         SYP/Dist-Zilla-MintingProfile-SYP-0.006.tar.gz
    gitpan-cpan-author:       SYP
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/MintingProfile/SYP.pm
+++ lib/Dist/Zilla/MintingProfile/SYP.pm
@@ -8,7 +8,7 @@ use warnings qw(all);
 use Moose;
 with q(Dist::Zilla::Role::MintingProfile::ShareDir);
 
-our $VERSION = '0.005'; # VERSION
+our $VERSION = '0.006'; # VERSION
 
 no Moose;
 __PACKAGE__->meta->make_immutable;
@@ -27,7 +27,7 @@ Dist::Zilla::MintingProfile::SYP - SYP's Dist::Zilla minting profile
 
 =head1 VERSION
 
-version 0.005
+version 0.006
 
 =head1 AUTHOR
 
